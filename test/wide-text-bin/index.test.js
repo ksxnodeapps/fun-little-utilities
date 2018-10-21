b77c3491df33540cf0a49b005ef0f73bf2116d6e
khai96_ <hvksmr1996@gmail.com>
2018-10-21 18:04:37 +0700

    Add more tests to wide-text-bin

--- test/wide-text-bin/index.test.js
+++ test/wide-text-bin/index.test.js
@@ -32,4 +32,22 @@ describe('when call', () => {
     const received = getReceived()
     expect(received).toEqual(expected)
   })
+
+  it('with specified charSep', () => {
+    const expected = getExpected({ charSep: 3 })
+    const received = getReceived(['--charSep=3'])
+    expect(received).toEqual(expected)
+  })
+
+  it('with specified wordSep', () => {
+    const expected = getExpected({ wordSep: 6 })
+    const received = getReceived(['--wordSep=6'])
+    expect(received).toEqual(expected)
+  })
+
+  it('with specified charSep and wordSep', () => {
+    const expected = getExpected({ charSep: 3, wordSep: 6 })
+    const received = getReceived(['--charSep=3', '--wordSep=6'])
+    expect(received).toEqual(expected)
+  })
 })
