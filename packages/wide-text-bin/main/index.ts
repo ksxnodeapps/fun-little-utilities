68b91a4c0e1db66fe2e96b9cad3a62787a547d1c
khai96_ <hvksmr1996@gmail.com>
2018-10-21 18:12:13 +0700

    Change help message

--- packages/wide-text-bin/main/index.ts
+++ packages/wide-text-bin/main/index.ts
@@ -5,13 +5,13 @@ const { argv } = yargs
   .usage('$0 <text> [options]')
   .option('charSep', {
     alias: 'c',
-    describe: 'Size of separator between characters in a word',
+    describe: 'Number of spaces between characters in a word',
     type: 'number',
     default: 1
   })
   .option('wordSep', {
     alias: 'w',
-    describe: 'Size of separator between words',
+    describe: 'Number of spaces between words',
     type: 'number',
     default: 2
   })
