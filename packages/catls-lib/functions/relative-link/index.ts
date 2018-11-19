3ae0cc6ea5fe7703fdd48f2e4401b7a3fca62d84
khai96_ <hvksmr1996@gmail.com>
2018-11-19 23:31:18 +0700

    Consider root

--- packages/catls-lib/functions/relative-link/index.ts
+++ packages/catls-lib/functions/relative-link/index.ts
@@ -5,15 +5,23 @@ import {
 } from 'path'
 
 function relativeLink (link: string, target: string): string {
-  const [first, ...rest] = target.split(/\/|\\/)
+  const [first] = target.split(/\/|\\/)
 
-  switch (first) {
-    case '.':
-      return relativeLink(link, rest.join(sep))
-    case '..':
-      return relativeLink(dirname(link), rest.join(sep))
-    default:
-      return join(dirname(link), target)
+  return first === '' || /a-z+:/.test(first)
+    ? target
+    : notRoot(link, target)
+
+  function notRoot (link: string, target: string): string {
+    const [first, ...rest] = target.split(/\/|\\/)
+
+    switch (first) {
+      case '.':
+        return notRoot(link, rest.join(sep))
+      case '..':
+        return notRoot(dirname(link), rest.join(sep))
+      default:
+        return join(dirname(link), target)
+    }
   }
 }
 
