0b55760808fe099de9cc1c995cb4ee2a6b23da76
khai96_ <hvksmr1996@gmail.com>
2018-11-25 00:25:24 +0700

    Add tests

--- test/split-shell-buffer/index.test.ts
+++ test/split-shell-buffer/index.test.ts
@@ -5,7 +5,18 @@ const normalText = [
   'jkl mno pqrs'
 ].join('\n')
 
-it('correctly indents', () => {
+const styledText = [
+  'abc \x1B[1mdef \x1B[2m',
+  'ghi \x1B[0;0mjkl \x1B[91;22;3m',
+  'mno pqrs \x1B[m',
+  'tuv wxyz',
+  '01 \x1B[2m23 \x1B[1m45',
+  '67 \x1B[m89',
+  'foo',
+  'bar'
+].join('\n')
+
+it('correctly indents normal text', () => {
   const indentedNormalText = [
     '  abc def ghi',
     '  jkl mno pqrs'
@@ -21,4 +32,13 @@ it('correctly indents', () => {
       .fromString(indentedNormalText)
       .toString()
   )
+})
+
+it('correctly indents styled text', () => {
+  expect(
+    Splitter
+      .fromString(styledText)
+      .withIndent(2)
+      .toString()
+  ).toMatchSnapshot()
 })
