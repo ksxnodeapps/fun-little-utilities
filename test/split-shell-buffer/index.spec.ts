0f5003e58e8b1416643115dfb15e9b77978d0b88
khai96_ <hvksmr1996@gmail.com>
2018-11-25 00:13:47 +0700

    Update tests

--- test/split-shell-buffer/index.spec.ts
+++ test/split-shell-buffer/index.spec.ts
@@ -9,7 +9,11 @@ const styledText = [
   'abc \x1B[1mdef \x1B[2m',
   'ghi \x1B[0;0mjkl \x1B[91;22;3m',
   'mno pqrs \x1B[m',
-  'tuv wxyz'
+  'tuv wxyz',
+  '01 \x1B[2m23 \x1B[1m45',
+  '67 \x1B[m89',
+  'foo',
+  'bar'
 ].join('\n')
 
 describe('constructor', () => {
