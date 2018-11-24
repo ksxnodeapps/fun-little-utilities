bbd1c538d5a42bc35adf36004bbb487eb5408a0b
khai96_ <hvksmr1996@gmail.com>
2018-11-25 00:31:06 +0700

    Update preview

--- test/split-shell-buffer/preview.ts
+++ test/split-shell-buffer/preview.ts
@@ -1,7 +1,14 @@
 import Splitter from 'split-shell-buffer'
 import { styledText } from './.lib/data'
 
+console.info('ORIGINAL')
 console.info(styledText)
 console.info()
+
+console.info('SPLITTED')
 console.info(Splitter.fromString(styledText).toString())
+console.info()
+
+console.info('SPLITTED AND INDENTED')
+console.info(Splitter.fromString(styledText).withIndent(4).toString())
 console.info()
