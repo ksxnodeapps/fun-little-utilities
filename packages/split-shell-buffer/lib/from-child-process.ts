ae47b839351faedd6f52797eae02d5beffe0bda3
khai96_ <hvksmr1996@gmail.com>
2018-11-27 18:16:20 +0700

    Use switch-case
    Events other than 'close', 'error' and 'data' are ignored

--- packages/split-shell-buffer/lib/from-child-process.ts
+++ packages/split-shell-buffer/lib/from-child-process.ts
@@ -7,16 +7,22 @@ function fromChildProcess (cp: types.ChildProcess): SplitterObject {
   const { stdout, stderr } = cp
 
   const stream: types.EventedStream = {
-    on (event: any, fn: any) {
-      if (event === 'close') {
-        cp.on('close', fn)
-      } else if (event === 'error') {
-        cp.on('error', fn)
-        stdout.on('error', error => fn(new StdOutError(stdout, error)))
-        stderr.on('error', error => fn(new StdErrError(stderr, error)))
-      } else {
-        stdout.on(event, fn)
-        stderr.on(event, fn)
+    on (event: any, fn: any): void {
+      switch (event) {
+        case 'close':
+          cp.on('close', fn)
+          break
+
+        case 'error':
+          cp.on('error', fn)
+          stdout.on('error', error => fn(new StdOutError(stdout, error)))
+          stderr.on('error', error => fn(new StdErrError(stderr, error)))
+          break
+
+        case 'data':
+          stdout.on(event, fn)
+          stderr.on(event, fn)
+          break
       }
     }
   }
