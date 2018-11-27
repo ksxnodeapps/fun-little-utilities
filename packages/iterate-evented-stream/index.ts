11f5f1f2ea080f1c8dce2f689954bfbc0a47cd7a
khai96_ <hvksmr1996@gmail.com>
2018-11-27 22:07:31 +0700

    Make generic parameter 'Err' optional

--- packages/iterate-evented-stream/index.ts
+++ packages/iterate-evented-stream/index.ts
@@ -64,20 +64,20 @@ export function iterate<Chunk, Err = any> (
   return iterate(0)
 }
 
-export interface EventedStream<Chunk, Err> {
+export interface EventedStream<Chunk, Err = any> {
   readonly addListener: EventedStream.ListenerModifier<Chunk, Err>
   readonly removeListener: EventedStream.ListenerModifier<Chunk, Err>
 }
 
 export namespace EventedStream {
-  export interface ListenerModifier<Chunk, Err> {
+  export interface ListenerModifier<Chunk, Err = any> {
     (event: 'data', listener: DataEventListener<Chunk>): void
     (event: 'error', listener: ErrorEventListener<Err>): void
     (event: 'close', listener: CloseEventListener): void
   }
 
   export type DataEventListener<Chunk> = (chunk: Chunk) => void
-  export type ErrorEventListener<Err> = (error: Err) => void
+  export type ErrorEventListener<Err = any> = (error: Err) => void
   export type CloseEventListener = () => void
 }
 
