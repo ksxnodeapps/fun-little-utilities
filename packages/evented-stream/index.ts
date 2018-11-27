8a37a5d4d7dfcc7c887dbda5e1e58111a2a38b9d
khai96_ <hvksmr1996@gmail.com>
2018-11-28 00:46:10 +0700

    Create evented-stream

---
+++ packages/evented-stream/index.ts
@@ -0,0 +1,7 @@
+import * as types from 'evented-stream-types'
+import iterateEventedStream from 'iterate-evented-stream'
+
+export * from 'evented-stream-types'
+export { iterateEventedStream, types }
+
+export const iterate = iterateEventedStream
