d1c91a86185744fa07833141fd67ac18a9fc7d29
khai96_ <hvksmr1996@gmail.com>
2018-12-04 11:30:41 +0700

    Shouldn't be parallel

--- packages/catls-lib/functions/main/index.ts
+++ packages/catls-lib/functions/main/index.ts
@@ -41,61 +41,63 @@ async function main (param: Main.Param): Promise<number> {
   const actualFollowSymlink = num2str(followSymlink)
   const { getLink, getLoop, getStat } = symlinkRoutingFunctions(symlinkResolution)
   const execute = executor(dontFakeInteractive)
+  let currentStatus = 0
 
-  const statusCodeList = await Promise.all(list.map(name => unit({
-    followSymlink: actualFollowSymlink,
+  for (const name of list) {
+    const statusAddend = await unit({
+      followSymlink: actualFollowSymlink,
 
-    handleNonExist (param) {
-      stderr.write(`[ERROR] No such file or directory: ${param.options.name}`)
-      return ExitStatus.NoEnt
-    },
+      handleNonExist (param) {
+        stderr.write(`[ERROR] No such file or directory: ${param.options.name}`)
+        return ExitStatus.NoEnt
+      },
 
-    async handleSymlink (param) {
-      stdout.write(statInfo('Symbolic Link', param.stats, [
-        ['Target', param.target],
-        ['Data', await readlink(param.options.name)] // don't use param.content for this
-      ]))
+      async handleSymlink (param) {
+        stdout.write(statInfo('Symbolic Link', param.stats, [
+          ['Target', param.target],
+          ['Data', await readlink(param.options.name)] // don't use param.content for this
+        ]))
 
-      return 0
-    },
+        return 0
+      },
 
-    handleFile (param) {
-      stdout.write(statInfo('File', param.stats))
-      return showExecData({
-        cmd: cat,
-        args: [...catArguments, param.options.name],
-        writable: stdout,
-        execute
-      })
-    },
+      handleFile (param) {
+        stdout.write(statInfo('File', param.stats))
+        return showExecData({
+          cmd: cat,
+          args: [...catArguments, param.options.name],
+          writable: stdout,
+          execute
+        })
+      },
 
-    handleDirectory (param) {
-      stdout.write(statInfo('Directory', param.stats))
-      stdout.write('[CONTENT]\n')
-      return showExecData({
-        cmd: ls,
-        args: [...lsArguments, param.options.name],
-        writable: stdout,
-        execute
-      })
-    },
+      handleDirectory (param) {
+        stdout.write(statInfo('Directory', param.stats))
+        stdout.write('[CONTENT]\n')
+        return showExecData({
+          cmd: ls,
+          args: [...lsArguments, param.options.name],
+          writable: stdout,
+          execute
+        })
+      },
 
-    handleUnknown ({ stats }) {
-      stdout.write(statInfo(unknownStatType(stats), stats))
-      return 0
-    },
+      handleUnknown ({ stats }) {
+        stdout.write(statInfo(unknownStatType(stats), stats))
+        return 0
+      },
 
-    getLink,
-    getStat,
-    getLoop,
-    name,
-    addStatusCode
-  })))
+      getLink,
+      getStat,
+      getLoop,
+      name,
+      addStatusCode
+    })
 
-  return statusCodeList.reduce(
-    async (prev, current) => addStatusCode(await prev, current),
-    Promise.resolve(0)
-  )
+    currentStatus = await addStatusCode(currentStatus, statusAddend)
+  }
+
+  return currentStatus
 }
 
 export = main
