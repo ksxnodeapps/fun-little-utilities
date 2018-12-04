26b15ffe4e7cd0f2512a9bc28d25d7401a5bf8f4
khai96_ <hvksmr1996@gmail.com>
2018-12-04 11:39:00 +0700

    Use writeln() instead of iteration of lines()

--- packages/catls-lib/functions/show-exec-data/index.ts
+++ packages/catls-lib/functions/show-exec-data/index.ts
@@ -1,15 +1,11 @@
-import { lines, fromChildProcess } from 'split-shell-buffer'
+import { writeln, fromChildProcess } from 'split-shell-buffer'
 import { ShowExecData } from '../../types'
 
 async function showExecData (param: ShowExecData.Param): Promise<number> {
   const { cmd, args, execute, writable } = param
   const cp = await execute(cmd, args)
   const splitter = fromChildProcess(cp).withIndent(2)
-
-  for await (const buffer of lines(splitter)) {
-    writable.write(Buffer.from(Array.from(buffer)))
-  }
-
+  await writeln(writable, splitter)
   return new Promise<number>(resolve => cp.once('close', resolve))
 }
 
