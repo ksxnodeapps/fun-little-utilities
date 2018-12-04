3611fbc9d77ed6121c5b01c2958bc715e50d4fa9
khai96_ <hvksmr1996@gmail.com>
2018-12-04 12:20:47 +0700

    Add more to preview

--- test/split-shell-buffer/preview.ts
+++ test/split-shell-buffer/preview.ts
@@ -1,23 +1,62 @@
 import process from 'process'
-import { fromString, toString, SequenceFunc } from 'split-shell-buffer'
+import { spawn } from 'child_process'
+import { fromString, toString, SequenceFunc, fromChildProcess, writeln } from 'split-shell-buffer'
 import { styledText } from './.lib/data'
+const executable = require.resolve('./.data/executable')
+
+function execute () {
+  return spawn('node', [executable])
+}
 
 async function main () {
   console.info('STYLED TEXT')
   console.info(styledText)
   console.info()
 
-  console.info('STYLED TEXT → SPLITTED')
+  console.info('STYLED TEXT → SPLITTED → TO STRING')
   console.info(await toString(fromString(styledText)))
   console.info()
 
-  console.info('STYLED TEXT → SPLITTED AND INDENTED')
+  console.info('STYLED TEXT → SPLITTED AND INDENTED → TO STRING')
   console.info(await toString(fromString(styledText).withIndent(4)))
   console.info()
 
-  console.info('STYLED TEXT → SPLITTED AND NUMBERED')
+  console.info('STYLED TEXT → SPLITTED AND NUMBERED → TO STRING')
   console.info(await toString(fromString(styledText).withPrefix(getNumberingPrefix())))
   console.info()
+
+  console.info('STYLED TEXT → SPLITTED → WRITELN')
+  await writeln(process.stdout, fromString(styledText))
+  console.info()
+
+  console.info('STYLED TEXT → SPLITTED AND INDENTED → WRITELN')
+  await writeln(process.stdout, fromString(styledText).withIndent(4))
+  console.info()
+
+  console.info('STYLED TEXT → SPLITTED AND NUMBERED → WRITELN')
+  await writeln(process.stdout, fromString(styledText).withPrefix(getNumberingPrefix()))
+  console.info()
+
+  console.info('SPAWNED')
+  require(executable)
+  console.info()
+
+  console.info('SPAWNED → SPLITTED AND INDENTED → TO STRING')
+  console.info(await toString(fromChildProcess(execute()).withIndent(4)))
+  console.info()
+
+  console.info('SPAWNED → SPLITTED AND NUMBERED → TO STRING')
+  console.info(await toString(fromChildProcess(execute()).withPrefix(getNumberingPrefix())))
+  console.info()
+
+  console.info('SPAWNED → SPLITTED AND INDENTED → WRITELN')
+  await writeln(process.stdout, fromChildProcess(execute()).withIndent(4))
+  console.info()
+
+  console.info('SPAWNED → SPLITTED AND NUMBERED → WRITELN')
+  await writeln(process.stdout, fromChildProcess(execute()).withPrefix(getNumberingPrefix()))
+  console.info()
+
 }
 
 function getNumberingPrefix (): SequenceFunc {
