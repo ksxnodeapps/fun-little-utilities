c387c5845624e6c0a9724b57e0cfe70ccb1d489f
khai96_ <hvksmr1996@gmail.com>
2018-12-04 17:44:25 +0700

    Create ts-await-event

---
+++ packages/ts-await-event/index.ts
@@ -0,0 +1,21 @@
+function awaitEvent<Event, Param> (
+  target: awaitEvent.Target<Event, Param>,
+  event: Event,
+): Promise<Param> {
+  return new Promise<Param>(
+    resolve =>
+      target.once(event, param => resolve(param))
+  )
+}
+
+namespace awaitEvent {
+  export interface Target<Event, Param> {
+    once (event: Event, listener: Listener<Param>): void
+  }
+
+  export interface Listener<Param> {
+    (param: Param): void
+  }
+}
+
+export = awaitEvent
---
+++ test/ts-await-event/typecheck.ts
@@ -0,0 +1,17 @@
+// tslint:disable:no-floating-promises
+
+import assert from 'static-type-assert'
+import awaitEvent from 'ts-await-event'
+
+type Event = 'Event'
+type Param = 'Param'
+
+const target = {
+  once (event: Event, listener: (param: Param) => void): void {
+    console.log({ event, listener })
+  }
+}
+
+assert<
+  Promise<Param>
+>(awaitEvent(target, 'Event'))
