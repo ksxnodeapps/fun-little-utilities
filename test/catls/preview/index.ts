2b624e0d24210cb2367ad551b3d5e6ebd5674d55
khai96_ <hvksmr1996@gmail.com>
2018-12-05 13:25:42 +0700

    Move folder tree out of project directory

--- test/catls/preview/index.ts
+++ test/catls/preview/index.ts
@@ -2,44 +2,52 @@ import process from 'process'
 import { spawn } from 'child_process'
 import { fromChildProcess, writeln } from 'split-shell-buffer'
 import event2promise from 'ts-await-event'
+import createTree from './utils/fs-tree-factory'
 const script = require.resolve('./executable')
 
-async function execute (...args: string[]): Promise<void> {
-  console.info(`\n$ catls ${args.join(' ')}`)
-
-  const cp = spawn(
-    'node',
-    [script, ...args],
-    {
-      cwd: __dirname
-    }
-  )
-
-  const closeEventPromise = event2promise<'close', number>(cp, 'close')
-  const splitter = fromChildProcess(cp).withPrefix(Buffer.from('  | '))
-  await writeln(process.stdout, splitter)
-
-  const status = await closeEventPromise
-  console.info(`  status: ${status}`)
-
-  console.info()
-}
-
 async function main (): Promise<void> {
+  async function execute (...args: string[]): Promise<void> {
+    console.info(`\n$ catls ${args.join(' ')}`)
+
+    const cp = spawn(
+      'node',
+      [script, ...args],
+      {
+        env: {
+          ...process.env,
+          WDIR: tree.targetPath
+        }
+      }
+    )
+
+    const closeEventPromise = event2promise<'close', number>(cp, 'close')
+    const splitter = fromChildProcess(cp).withPrefix(Buffer.from('  | '))
+    await writeln(process.stdout, splitter)
+
+    const status = await closeEventPromise
+    console.info(`  status: ${status}`)
+
+    console.info()
+  }
+
+  const tree = await createTree()
+
   await execute()
   await execute('--help')
 
   await execute(
     '--noScript',
-    'data.tsignore/folder/foo.txt',
-    'data.tsignore/folder/bar.txt',
-    'data.tsignore/folder/baz.txt'
+    'data/folder/foo.txt',
+    'data/folder/bar.txt',
+    'data/folder/baz.txt'
   )
   await execute(
-    'data.tsignore/folder/foo.txt',
-    'data.tsignore/folder/bar.txt',
-    'data.tsignore/folder/baz.txt'
+    'data/folder/foo.txt',
+    'data/folder/bar.txt',
+    'data/folder/baz.txt'
   )
+
+  await tree.destroy()
 }
 
 main().catch(error => {
---
+++ test/catls/preview/utils/fs-tree-factory.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import * as fsx from 'fs-extra'
+import * as fsTreeUtils from 'fs-tree-utils'
+import tempPath from 'unique-temp-path'
+const { Clone, Symlink } = fsTreeUtils.FileSystemRepresentation
+const existingDataPath = path.resolve(__dirname, '../data')
+
+async function create () {
+  const targetPath = tempPath()
+
+  const targetTree = {
+    data: {
+      folder: new Clone(path.join(existingDataPath, 'folder')),
+      link: {
+        'to-container': new Symlink('.'),
+        'to-parent': new Symlink('..'),
+        'to-data': new Symlink('../..'),
+        'to-folder': new Symlink('../folder')
+      },
+      link2: {
+        'to-to-container': new Symlink('../link/to-container'),
+        'to-to-parent': new Symlink('../link/to-parent'),
+        'to-to-data': new Symlink('../link/to-data'),
+        'to-to-folder': new Symlink('../link/to-folder')
+      },
+      linkr: {
+        a: new Symlink('d'),
+        b: new Symlink('a'),
+        c: new Symlink('b'),
+        d: new Symlink('c')
+      }
+    }
+  }
+
+  await fsTreeUtils.create(targetTree, targetPath)
+
+  const destroy = () => fsx.remove(targetPath)
+
+  return {
+    targetPath,
+    targetTree,
+    destroy
+  }
+}
+
+export = create
