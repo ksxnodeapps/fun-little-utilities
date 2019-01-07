06506cc4cfc2d5e10c19dd1a24b1005356a41305
khai96_ <hvksmr1996@gmail.com>
2019-01-07 21:39:46 +0700

    Save it for now

---
+++ test/catls-lib/.lib/fake-file-system.ts
@@ -0,0 +1,128 @@
+import { Main, UnitType, StatInfo } from 'catls-lib'
+import FakeStats from './fake-stats'
+
+const symDict = Symbol('symDict')
+const symStatInfo = Symbol('symStatInfo')
+const symMkStats = Symbol('symMkFakeStats')
+
+class FileSystemInstanceBase {
+  protected readonly [symDict]: FileSystemInstance.Dict
+  protected readonly [symStatInfo]: StatInfo.Stats
+
+  constructor (options: FileSystemInstance.ConstructorOptions) {
+    const { dict, statInfo } = options
+    this[symDict] = dict
+    this[symStatInfo] = statInfo
+  }
+
+  protected [symMkStats] (type: FakeStats.Type): FakeStats {
+    const statInfo = this[symStatInfo]
+    return new FakeStats(
+      type,
+      statInfo.size,
+      statInfo.mode,
+      statInfo.atime,
+      statInfo.ctime,
+      statInfo.mtime
+    )
+  }
+}
+
+class FileSystemInstance extends FileSystemInstanceBase implements Main.FileSystemFunctions {
+  public readonly existsSync = (name: string) => {
+    const item = this[symDict][name]
+    if (!item) return false
+    if (item.type === UnitType.NonExist) return false
+    return true
+  }
+
+  public readonly stat = (name: string) => {
+    if (!this.existsSync(name)) {
+      throw new Error(`ENOENT: No such file or directory, stat '${name}'`)
+    }
+
+    const item = this[symDict][name]
+
+    return item.type === UnitType.Symlink
+      ? this.stat(item.content)
+      : this[symMkStats](item.type)
+  }
+
+  public readonly lstat = (name: string) => this[symMkStats](this[symDict][name].type)
+
+  public readonly readlink = (name: string) => {
+    const item = this[symDict][name]
+
+    if (item.type !== UnitType.Symlink) {
+      throw new Error(`EINVAL: invalid argument, readlink '${name}'`)
+    }
+
+    return item.content
+  }
+
+  public readonly realpath = (name: string) => {
+    const item = this[symDict][name]
+    return item.type === UnitType.Symlink
+      ? this.realpath(item.content)
+      : name
+  }
+}
+
+namespace FileSystemInstance {
+  export interface ConstructorOptions {
+    readonly dict: Dict
+    readonly statInfo: StatInfo.Stats
+  }
+
+  export type Dict = {
+    readonly [name: string]: Item
+  }
+
+  export type ItemType = UnitType
+  export const ItemType = UnitType
+
+  function itemClassWithoutContent<Type extends ItemType> (type: Type) {
+    class ItemInstance extends ItemBase<Type> {
+      constructor (name: string) {
+        super(name, type)
+      }
+    }
+
+    return ItemInstance
+  }
+
+  function itemClassWithContent<Type extends ItemType> (type: Type) {
+    class ItemInstance<Content> extends itemClassWithoutContent(type) {
+      constructor (
+        name: string,
+        public readonly content: Content
+      ) {
+        super(name)
+      }
+    }
+
+    return ItemInstance
+  }
+
+  export type Item =
+    NonExist |
+    Symlink |
+    File |
+    Directory |
+    Unknown
+
+  export abstract class ItemBase<Type extends ItemType> {
+    constructor (
+      public readonly name: string,
+      public readonly type: Type
+    ) {}
+  }
+
+  export class NonExist extends itemClassWithoutContent(ItemType.NonExist) {}
+  export class Symlink extends itemClassWithContent(ItemType.Symlink)<string> {}
+  export class File extends itemClassWithContent(ItemType.File)<string> {}
+  export class Directory extends itemClassWithContent(ItemType.Directory)<ReadonlyArray<string>> {}
+  export class Unknown extends itemClassWithoutContent(ItemType.Unknown) {}
+}
+
+export = FileSystemInstance
---
+++ test/catls-lib/.lib/fake-stats.ts
@@ -0,0 +1,55 @@
+import { Main, StatInfo, UnitType, UnknownStatTypeName } from 'catls-lib'
+
+const symType = Symbol()
+const symMkFn = Symbol()
+const confirm = (): true => true
+const deny = (): false => false
+
+class Base {
+  private readonly [symType]: StatsInstance.Type
+
+  constructor (type: StatsInstance.Type) {
+    this[symType] = type
+    return Object.create(this)
+  }
+
+  protected [symMkFn] (expected: StatsInstance.Type) {
+    return expected === this[symType] ? confirm : deny
+  }
+}
+
+class StatsInfoInstance extends Base implements StatInfo.Stats {
+  constructor (
+    type: StatsInstance.Type,
+    public readonly size: number,
+    public readonly mode: number,
+    public readonly atime: StatInfo.Stats.Date,
+    public readonly ctime: StatInfo.Stats.Date,
+    public readonly mtime: StatInfo.Stats.Date
+  ) {
+    super(type)
+  }
+}
+
+class StatsInstance extends StatsInfoInstance implements Main.Stats {
+  public readonly isSymbolicLink = this[symMkFn](Type.Symlink)
+  public readonly isFile = this[symMkFn](Type.File)
+  public readonly isDirectory = this[symMkFn](Type.Directory)
+  public readonly isBlockDevice = this[symMkFn](Type.BlockDevice)
+  public readonly isCharacterDevice = this[symMkFn](Type.CharacterDevice)
+  public readonly isFIFO = this[symMkFn](Type.FIFO)
+  public readonly isSocket = this[symMkFn](Type.Socket)
+}
+
+namespace StatsInstance {
+  export interface ConstructorOptions extends StatInfo.Stats {
+    readonly type: Type
+  }
+
+  export type Type = UnitType | UnknownStatTypeName
+  export const Type = { ...UnitType, ...UnknownStatTypeName }
+}
+
+const { Type } = StatsInstance
+
+export = StatsInstance
