dc13b2f1d3f646b3385ba2ec91cc8f02d39dfea0
khai96_ <hvksmr1996@gmail.com>
2019-01-11 03:01:14 +0700

    Move repetitive tasks into functions

--- test/catls-lib/functions/unit.test.ts
+++ test/catls-lib/functions/unit.test.ts
@@ -8,32 +8,26 @@ const init = (
   symlinkResolution = SymlinkResolution.Agnostic
 ) => {
   type ResName = keyof typeof HandlerReturn
+  type Key = keyof UnitParam
   const param = new UnitParam({ name, followSymlink, symlinkResolution, fsPromise })
   const promise = unit(param)
   const resname: Promise<ResName> = promise.then(x => HandlerReturn[x] as any)
   const mkfn = (fn: (x: ResName) => void) => async () => fn(await resname)
-  const ignore = (name: keyof UnitParam) => mkfn(() => expect(param[name]).not.toBeCalled())
-  return { param, promise, resname, mkfn, ignore }
+  const calledOnce = (name: Key) => mkfn(() => expect(param[name]).toBeCalledTimes(1))
+  const calledWith = (name: Key, args: any[]) => mkfn(() => expect(param[name]).toBeCalledWith(...args))
+  const returns = (code: HandlerReturn) => mkfn(name => expect(name).toBe(HandlerReturn[code]))
+  const ignore = (name: Key) => mkfn(() => expect(param[name]).not.toBeCalled())
+  return { param, promise, resname, mkfn, calledOnce, calledWith, returns, ignore }
 }
 
 describe('with something that does not exist', () => {
-  const { param, mkfn, ignore } = init('not exist')
-
-  it('calls options.handleNonExist once', mkfn(() => {
-    expect(param.handleNonExist).toBeCalledTimes(1)
-  }))
-
-  it('calls options.handleNonExist with expected arguments', mkfn(() => {
-    expect(param.handleNonExist).toBeCalledWith({
-      type: UnitType.NonExist,
-      options: param
-    })
-  }))
-
-  it('returns expected value', mkfn(value => {
-    expect(value).toBe(HandlerReturn[HandlerReturn.NonExist])
-  }))
-
+  const { param, calledOnce, calledWith, returns, ignore } = init('not exist')
+  it('calls options.handleNonExist once', calledOnce('handleNonExist'))
+  it('calls options.handleNonExist with expected arguments', calledWith('handleNonExist', [{
+    type: UnitType.NonExist,
+    options: param
+  }]))
+  it('returns expected value', returns(HandlerReturn.NonExist))
   it('does not call handleSymlink', ignore('handleSymlink'))
   it('does not call handleFile', ignore('handleFile'))
   it('does not call handleDirectory', ignore('handleDirectory'))
@@ -41,24 +35,14 @@ describe('with something that does not exist', () => {
 })
 
 describe('with a file', () => {
-  const { param, mkfn, ignore } = init('simple file')
-
-  it('calls options.handleFile once', mkfn(() => {
-    expect(param.handleFile).toBeCalledTimes(1)
-  }))
-
-  it('calls options.handleFile with expected arguments', mkfn(() => {
-    expect(param.handleFile).toBeCalledWith({
-      type: UnitType.File,
-      options: param,
-      stats: getStatsPattern('simple file')
-    })
-  }))
-
-  it('returns expected value', mkfn(value => {
-    expect(value).toBe(HandlerReturn[HandlerReturn.File])
-  }))
-
+  const { param, calledOnce, calledWith, returns, ignore } = init('simple file')
+  it('calls handleFile once', calledOnce('handleFile'))
+  it('calls handleFile with expected arguments', calledWith('handleFile', [{
+    type: UnitType.File,
+    options: param,
+    stats: getStatsPattern('simple file')
+  }]))
+  it('returns expected value', returns(HandlerReturn.File))
   it('does not call handleNonExist', ignore('handleNonExist'))
   it('does not call handleSymlink', ignore('handleSymlink'))
   it('does not call handleDirectory', ignore('handleDirectory'))
@@ -66,24 +50,14 @@ describe('with a file', () => {
 })
 
 describe('with a directory', () => {
-  const { param, mkfn, ignore } = init('simple directory')
-
-  it('calls options.handleDirectory once', mkfn(() => {
-    expect(param.handleDirectory).toBeCalledTimes(1)
-  }))
-
-  it('calls options.handleDirectory with expected arguments', mkfn(() => {
-    expect(param.handleDirectory).toBeCalledWith({
-      type: UnitType.Directory,
-      options: param,
-      stats: getStatsPattern('simple directory')
-    })
-  }))
-
-  it('returns expected value', mkfn(value => {
-    expect(value).toBe(HandlerReturn[HandlerReturn.Directory])
-  }))
-
+  const { param, calledOnce, calledWith, returns, ignore } = init('simple directory')
+  it('calls handleDirectory once', calledOnce('handleDirectory'))
+  it('calls handleDirectory with expected arguments', calledWith('handleDirectory', [{
+    type: UnitType.Directory,
+    options: param,
+    stats: getStatsPattern('simple directory')
+  }]))
+  it('returns expected value', returns(HandlerReturn.Directory))
   it('does not call handleNonExist', ignore('handleNonExist'))
   it('does not call handleSymlink', ignore('handleSymlink'))
   it('does not call handleFile', ignore('handleFile'))
