d4458b8d2fdfd9a02c5c1b9384b2f9036f21e3b8
khai96_ <hvksmr1996@gmail.com>
2019-10-02 11:29:01 +0700

    Only allow JsonValue to be passed to formatJson

--- packages/string-template-format/format-json.ts
+++ packages/string-template-format/format-json.ts
@@ -1,7 +1,18 @@
 import Tag from './tag'
 
 /**
+ * Type of value to pass to `formatJson`
+ */
+export type JsonValue = JsonObject | JsonArray | null | boolean | number | string
+
+interface JsonObject {
+  readonly [_: string]: JsonValue
+}
+
+interface JsonArray extends Array<JsonValue> {}
+
+/**
  * Convert values to JSON string
  */
-export const formatJson = Tag(JSON.stringify)
+export const formatJson: Tag<JsonValue> = Tag(JSON.stringify)
 export default formatJson
