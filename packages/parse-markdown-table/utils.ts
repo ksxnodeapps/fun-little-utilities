e6ee0f799a0159e45c5712c273bf4e185e212899
khai96_ <hvksmr1996@gmail.com>
2020-01-13 16:45:17 +0700

    Fix

--- packages/parse-markdown-table/utils.ts
+++ packages/parse-markdown-table/utils.ts
@@ -1,22 +1,24 @@
 export type MaybeAsyncIterable<Value> = Iterable<Value> | AsyncIterable<Value>
 export type Stream = MaybeAsyncIterable<string>
 
-export async function * splitCharacterIterable (iterable: Stream, sepChar: string) {
+export async function * splitStream (iterable: Stream, sepChar: string) {
   let acc = ''
 
-  for await (const char of iterable) {
-    if (char === sepChar) {
-      yield acc
-      acc = ''
-    } else {
-      acc += char
+  for await (const chunk of iterable) {
+    for (const char of chunk) {
+      if (char === sepChar) {
+        yield acc
+        acc = ''
+      } else {
+        acc += char
+      }
     }
   }
 
   yield acc
 }
 
-export const iterateLines = (stream: Stream) => splitCharacterIterable(stream, '\n')
+export const iterateLines = (stream: Stream) => splitStream(stream, '\n')
 
 export async function * trimmedChunks (stream: Stream) {
   for await (const chunk of stream) {
