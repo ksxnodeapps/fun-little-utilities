8e0cd92fbde49f56ec0d985ad471589d9d688841
khai96_ <hvksmr1996@gmail.com>
2020-01-13 16:50:19 +0700

    Divide and Conquer

--- packages/parse-markdown-table/utils.ts
+++ packages/parse-markdown-table/utils.ts
@@ -1,22 +1,29 @@
 export type MaybeAsyncIterable<Value> = Iterable<Value> | AsyncIterable<Value>
 export type Stream = MaybeAsyncIterable<string>
 
-export async function * splitStream (iterable: Stream, sepChar: string) {
+export async function * iterateCharacters (stream: Stream) {
+  for await (const chunk of stream) {
+    yield * chunk
+  }
+}
+
+export async function * splitCharacterIterable (chars: MaybeAsyncIterable<string>, sepChar: string) {
   let acc = ''
 
-  for await (const chunk of iterable) {
-    for (const char of chunk) {
-      if (char === sepChar) {
-        yield acc
-        acc = ''
-      } else {
-        acc += char
-      }
+  for await (const char of chars) {
+    if (char === sepChar) {
+      yield acc
+      acc = ''
+    } else {
+      acc += char
     }
   }
 
   yield acc
 }
+
+export const splitStream = (stream: Stream, sepChar: string) =>
+  splitCharacterIterable(iterateCharacters(stream), sepChar)
 
 export const iterateLines = (stream: Stream) => splitStream(stream, '\n')
 
