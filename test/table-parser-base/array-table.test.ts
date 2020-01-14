1d13b903d3b9ef80f7d8deec7def1cd2a78bc4bd
khai96_ <hvksmr1996@gmail.com>
2020-01-14 12:06:15 +0700

    Create test for createArrayTable

---
+++ test/table-parser-base/array-table.test.ts
@@ -0,0 +1,35 @@
+import { ObjectTable, ListItem, createArrayTable } from 'table-parser-base'
+
+class Items<Item extends ListItem<string, any>> extends ObjectTable<string, any> {
+  constructor (
+    public readonly items: readonly Item[]
+  ) {
+    super()
+  }
+
+  public async * [Symbol.asyncIterator] () {
+    yield * this.items
+  }
+}
+
+it('no holes', async () => {
+  const items = new Items([
+    { id: 1, name: 'John Doe', email: 'john-doe@gmail.com' },
+    { id: 2, name: 'Peter Smith', email: 'petersmith22@outlook.com' },
+    { id: 3, name: 'Julia Jones', email: 'jjones778@gmail.com' }
+  ])
+
+  const table = await createArrayTable(items)
+  expect(table).toMatchSnapshot()
+})
+
+it('with holes', async () => {
+  const items = new Items([
+    { id: 1, name: 'John Doe', age: 32 },
+    { id: 2, name: 'Peter Smith', sex: 'male', alive: true },
+    { id: 3, name: 'Julia Jones', email: 'jjones778@gmail.com' }
+  ])
+
+  const table = await createArrayTable(items as any)
+  expect(table).toMatchSnapshot()
+})
