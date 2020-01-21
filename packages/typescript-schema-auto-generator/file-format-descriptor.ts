c5f90e05f1ae8a24ff7041d2ba1d669632b7709d
khai96_ <hvksmr1996@gmail.com>
2020-01-21 16:45:51 +0700

    Create descriptors for json and yaml

--- packages/typescript-schema-auto-generator/file-format-descriptor.ts
+++ packages/typescript-schema-auto-generator/file-format-descriptor.ts
@@ -1,4 +1,4 @@
-import { Result } from '@tsfun/result'
+import { Result, tryExec } from '@tsfun/result'
 import { Config } from './types'
 
 export interface FileFormatDescriptor {
@@ -14,4 +14,21 @@ export interface FileFormatDescriptor {
    * @param filename Path to the file
    */
   readonly parseConfigText: (text: string, filename: string) => Result<Config, unknown>
+}
+
+export async function createJsonFormatDescriptor (): Promise<FileFormatDescriptor> {
+  const { extname } = await import('path')
+  const EXT = ['.json', '']
+  const testFileName = (filename: string) => EXT.includes(extname(filename))
+  const parseConfigText = (text: string) => tryExec(() => JSON.parse(text))
+  return { testFileName, parseConfigText }
+}
+
+export async function createYamlFormatDescriptor (): Promise<FileFormatDescriptor> {
+  const { extname } = await import('path')
+  const { safeLoad } = await import('js-yaml')
+  const EXT = ['.yaml', '.yml', '']
+  const testFileName = (filename: string) => EXT.includes(extname(filename))
+  const parseConfigText = (text: string, filename: string) => tryExec(() => safeLoad(text, { filename }))
+  return { testFileName, parseConfigText }
 }
