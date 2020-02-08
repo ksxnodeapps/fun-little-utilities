cefe06113df68e2497112b8831e54cf2eef17434
khai96_ <hvksmr1996@gmail.com>
2020-02-08 16:27:15 +0700

    Complete parallel implementation

--- packages/available-npm-name/lib/sequence.ts
+++ packages/available-npm-name/lib/sequence.ts
@@ -1,14 +1,27 @@
+import { EventEmitter } from 'events'
+import { EventedStream, iterate } from 'iterate-evented-stream'
 import { Status, Fetch } from './types'
 import { MaybeAsyncIterable } from './utils'
 import unit from './unit'
 
+class Helper extends EventEmitter implements EventedStream<sequence.Item> {}
+
 /** Test multiple package names */
-export async function * sequence (param: sequence.Param): AsyncGenerator<sequence.Item, void> {
+export async function * sequence (param: sequence.Param) {
   const { packageNames, registryUrl, fetch } = param
-  for await (const packageName of packageNames) {
-    const status = await unit({ packageName, registryUrl, fetch })
-    yield { packageName, status }
+  const helper = new Helper()
+  async function loop () {
+    let waiter: Promise<unknown> = Promise.resolve()
+    for await (const packageName of packageNames) {
+      const promise = unit({ packageName, registryUrl, fetch })
+        .then(status => helper.emit('data', { packageName, status } as sequence.Item))
+        .catch(error => helper.emit('error', error))
+      waiter = waiter.finally(() => promise)
+    }
+    return waiter
   }
+  void loop().finally(() => helper.emit('close'))
+  yield * iterate<sequence.Item>(helper)
 }
 
 export namespace sequence {
