d63452865eb73bdcf92f2e6885deb36d1f6eede8
khai96_ <hvksmr1996@gmail.com>
2020-02-08 14:51:54 +0700

    Expose parseStdIn

--- packages/available-npm-name/lib/index.ts
+++ packages/available-npm-name/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './constants'
 export * from './fmt'
 export * from './main'
+export * from './parse-stdin'
 export * from './sequence'
 export * from './types'
 export * from './unit'
