5bb3d2325388c4adb41251cb31cdb78630a40f4b
khai96_ <hvksmr1996@gmail.com>
2020-02-08 19:09:16 +0700

    Create test for available-npm-name

---
+++ test/available-npm-name/main.test.ts
@@ -0,0 +1,387 @@
+import { resolve } from 'url'
+import { ConsoleInstance, ActionType, getString } from 'simple-fake-console'
+import delay from 'simple-delay'
+
+import {
+  Status,
+  Process,
+  Console,
+  Fetch,
+  NPM_REGISTRY,
+  main
+} from 'available-npm-name'
+
+class FakeChunk implements Process.Chunk {
+  constructor (
+    private readonly text: string
+  ) {}
+  public toString () {
+    return this.text
+  }
+}
+
+class FakeStream implements Process.Stream {
+  private async * prvIterate () {
+    yield * [
+      '  abc', '\n',
+      'de', 'f \n',
+      'ghi\n',
+      '  jkl\n',
+      '@abc', '/def', '\n',
+      '', '  \n',
+      'mno\n',
+      '    \n  ',
+      'foo\n',
+      'b', 'a', 'r'
+    ].map(async (text, index) => {
+      await delay(index * 10)
+      return new FakeChunk(text)
+    })
+  }
+
+  public readonly [Symbol.asyncIterator] = jest.fn(() => this.prvIterate())
+}
+
+class FakeConsole implements Console.Mod {
+  public readonly core = new ConsoleInstance()
+  public readonly info = jest.fn(this.core.info)
+
+  public getString () {
+    return '\n' + getString({
+      console: this.core,
+      types: [ActionType.Info]
+    }) + '\n'
+  }
+}
+
+const availableNames = [
+  'abc',
+  'def',
+  '@abc/def'
+]
+
+const occupiedNames = [
+  'ghi',
+  'jkl'
+]
+
+const erroredNames = [
+  'mno',
+  'foo',
+  'bar'
+]
+
+const invalidNames = [
+  '!!foo',
+  'bar baz'
+]
+
+const hasUrl = (names: readonly string[], url: string) => names
+  .map(name => resolve(NPM_REGISTRY, encodeURIComponent(name)))
+  .includes(url)
+
+const fetchImpl: Fetch.Fn = async url => {
+  if (!url.startsWith(NPM_REGISTRY)) {
+    throw new Error(`URL are supposed to start with ${NPM_REGISTRY}: ${url}`)
+  }
+
+  if (hasUrl(availableNames, url)) return { status: 404 }
+  if (hasUrl(occupiedNames, url)) return { status: 200 }
+  if (hasUrl(erroredNames, url)) return { status: -1 }
+
+  throw new RangeError(`URL ${url} is unaccounted for`)
+}
+
+async function setup (args: readonly string[]) {
+  const console = new FakeConsole()
+  const fetch = jest.fn(fetchImpl)
+  const stdin = new FakeStream()
+  const result = await main({
+    argv: {
+      _: args,
+      registry: NPM_REGISTRY
+    },
+    process: {
+      stdin
+    },
+    console,
+    fetch
+  })
+  return { console, fetch, stdin, result }
+}
+
+describe('names are supplied via cli arguments', () => {
+  describe('all names are available', () => {
+    const args = [...availableNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Available', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Available)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all names are occupied', () => {
+    const args = [...occupiedNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Occupied', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Occupied)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all names are causing errors', () => {
+    const args = [...erroredNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.NetworkError', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.NetworkError)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all names are invalid', () => {
+    const args = [...invalidNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.InvalidName', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.InvalidName)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all names either available or invalid', () => {
+    const args = [...availableNames, ...invalidNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Available | Status.InvalidName', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Available | Status.InvalidName)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all names either occupied or causing errors', () => {
+    const args = [...occupiedNames, ...erroredNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Occupied | Status.NetworkError', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Occupied | Status.NetworkError)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('all status combined', () => {
+    const args = [...availableNames, ...occupiedNames, ...erroredNames, ...invalidNames]
+
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Available | Status.Occupied | Status.NetworkError | Status.InvalidName', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Available | Status.Occupied | Status.NetworkError | Status.InvalidName)
+    })
+
+    it('does not read stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator]).not.toBeCalled()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+})
+
+describe('names are supplied via stdin, all', () => {
+  const args = [] as const
+
+  describe('combine all status except InvalidName', () => {
+    it('messages', async () => {
+      const { console } = await setup(args)
+      expect(console.getString()).toMatchSnapshot()
+    })
+
+    it('returns Status.Available | Status.Occupied | Status.NetworkError', async () => {
+      const { result } = await setup(args)
+      expect(result).toBe(Status.Available | Status.Occupied | Status.NetworkError)
+    })
+
+    it('read from stdin', async () => {
+      const { stdin } = await setup(args)
+      expect(stdin[Symbol.asyncIterator].mock.calls).toMatchSnapshot()
+    })
+
+    it('calls fetch', async () => {
+      const { fetch } = await setup(args)
+      expect(fetch.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+
+    it('calls console.info', async () => {
+      const { console } = await setup(args)
+      expect(console.info.mock.calls).toMatchSnapshot()
+    })
+  })
+})
