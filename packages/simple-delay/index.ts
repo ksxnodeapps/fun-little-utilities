fa5f714a724a8584d46dca353ed81bd55e4b9fa4
khai96_ <hvksmr1996@gmail.com>
2020-02-08 17:21:34 +0700

    Create simple-delay

---
+++ packages/simple-delay/index.ts
@@ -0,0 +1,10 @@
+/** Create a promise that resolves after a certain amount of time */
+export const callSetTimeout = (
+  setTimeout: (fn: () => void, ms: number) => void,
+  delay: number
+) => new Promise<void>(resolve => setTimeout(() => resolve(), delay))
+
+/** Create a promise that resolves after a certain amount of time */
+export const simpleDelay = (delay: number) => callSetTimeout(setTimeout, delay)
+
+export default simpleDelay
---
+++ test/simple-delay/index.test.ts
@@ -0,0 +1,29 @@
+async function setup () {
+  const spy = jest.spyOn(globalThis, 'setTimeout')
+  const { simpleDelay } = await import('simple-delay')
+  const delay = 5
+  const result = await simpleDelay(delay)
+  return { spy, delay, result }
+}
+
+afterEach(() => jest.restoreAllMocks())
+
+it('calls setTimeout once', async () => {
+  const { spy } = await setup()
+  expect(spy).toBeCalledTimes(1)
+})
+
+it('calls setTimeout with expected arguments', async () => {
+  const { spy, delay } = await setup()
+  expect(spy).toBeCalledWith(expect.any(Function), delay)
+})
+
+it('resolves undefined', async () => {
+  const { result } = await setup()
+  expect(result).toBe(undefined)
+})
+
+it('exports simpleDelay as default', async () => {
+  const { default: defaultExport, simpleDelay } = await import('simple-delay')
+  expect(defaultExport).toBe(simpleDelay)
+})
