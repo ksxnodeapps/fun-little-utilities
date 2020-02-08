935686d2ff03a262c3dc1a0804fd79e1b5ccc8ed
khai96_ <hvksmr1996@gmail.com>
2020-02-08 15:03:20 +0700

    Create cli

---
+++ packages/available-npm-name/cli/main.ts
@@ -0,0 +1,31 @@
+import yargs from 'yargs'
+import fetch from 'node-fetch'
+import console from 'console'
+import process from 'process'
+import { main } from '../index'
+
+const { argv } = yargs
+  .usage('$0 [names]')
+  .option('registry', {
+    alias: 'r',
+    describe: 'Configure registry',
+    type: 'string',
+    default: 'https://registry.npmjs.org'
+  })
+  .example('$0 foo bar', 'Supply package names via CLI arguments')
+  .example('$0 < packages.txt', 'Supply package names via a text file where each line is a package name')
+  .example('$0', 'Supply package names by entering each one via terminal')
+  .env('AVAILABLE_NPM_NAME')
+  .help()
+
+main({
+  argv,
+  fetch,
+  console,
+  process
+})
+  .then(status => process.exit(status))
+  .catch(error => {
+    console.error(error)
+    return process.exit(-1)
+  })
