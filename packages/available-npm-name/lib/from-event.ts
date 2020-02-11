45249bf093fe94c13d1f1828ce504cc77ba73cf5
khai96_ <hvksmr1996@gmail.com>
2020-02-11 16:17:36 +0700

    Change parseInput to use newly created line-observable-rxjs

--- packages/available-npm-name/lib/from-event.ts
+++ packages/available-npm-name/lib/from-event.ts
@@ -1,13 +1,4 @@
-import { fromEventPattern } from 'rxjs'
-import { Event } from './types'
-
-/** Convert a node event target to an observable */
-export const fromEvent = <Type, Info> (
-  target: Event.Target<Type, Info>,
-  type: Type
-) => fromEventPattern<Info>(
-  listener => target.addListener(type, listener),
-  listener => target.removeListener(type, listener)
-)
-
-export default fromEvent
+export {
+  fromEvent,
+  fromEvent as default
+} from 'line-observable-rxjs'
--- packages/available-npm-name/lib/parse-input.ts
+++ packages/available-npm-name/lib/parse-input.ts
@@ -1,25 +1,14 @@
 import { Observable, from } from 'rxjs'
-import { share, take, takeUntil, map, filter, mergeMap, bufferWhen } from 'rxjs/operators'
+import { share, map, filter } from 'rxjs/operators'
+import { getLinesFromStream } from 'line-observable-rxjs'
 import { Process } from './types'
-import fromEvent from './from-event'
 
 /** Create an observable of input from either cli arguments or stdin stream */
 export function parseInput (param: parseInput.Param): Observable<string> {
   const { args, stdin } = param
   if (args.length) return from(args)
-  const $close = fromEvent<'close', void>(stdin, 'close')
-    .pipe(take(1))
-    .pipe(share())
-  const $data = fromEvent<'data', Process.Chunk>(stdin, 'data')
-    .pipe(takeUntil($close))
-    .pipe(mergeMap(chunk => from([...chunk.toString()])))
-    .pipe(share())
-  const $buffer = $data
-    .pipe(filter(x => x === '\n'))
-    .pipe(share())
-  const $result = $data
-    .pipe(bufferWhen(() => $buffer))
-    .pipe(map(chunk => chunk.join('').trim()))
+  const $result = getLinesFromStream(stdin)
+    .pipe(map(line => line.trim()))
     .pipe(filter(x => Boolean(x)))
     .pipe(share())
   return $result
