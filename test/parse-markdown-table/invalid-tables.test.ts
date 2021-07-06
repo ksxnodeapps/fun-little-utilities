00e6943a5c71e381ae8da7a8331b09e3adfd1f5f
khai96_ <hvksmr1996@gmail.com>
2021-07-06 10:23:31 +0700

    test: Invalid cases of `createMarkdownObjectTableSync`

--- test/parse-markdown-table/invalid-tables.test.ts
+++ test/parse-markdown-table/invalid-tables.test.ts
@@ -1,34 +1,36 @@
-import { createMarkdownObjectTable } from 'parse-markdown-table'
+import { tryExec } from '@tsfun/result'
+import { createMarkdownObjectTable, createMarkdownObjectTableSync } from 'parse-markdown-table'
 import { getAsyncArray } from './lib/async-array'
 
-const setup = async (text: string) => getAsyncArray(await createMarkdownObjectTable(text))
+describe('async', () => {
+  const setup = async (text: string) => getAsyncArray(await createMarkdownObjectTable(text))
 
-describe('empty string', () => {
-  const text = ''
+  describe('empty string', () => {
+    const text = ''
 
-  it('rejects with a SyntaxError', async () => {
-    await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    it('rejects with a SyntaxError', async () => {
+      await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', async () => {
+      await expect(setup(text)).rejects.toMatchSnapshot()
+    })
   })
 
-  it('matches snapshot', async () => {
-    await expect(setup(text)).rejects.toMatchSnapshot()
+  describe('string with naught but whitespaces', () => {
+    const text = '  \n  \t '
+
+    it('rejects with a SyntaxError', async () => {
+      await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', async () => {
+      await expect(setup(text)).rejects.toMatchSnapshot()
+    })
   })
-})
 
-describe('string with naught but whitespaces', () => {
-  const text = '  \n  \t '
-
-  it('rejects with a SyntaxError', async () => {
-    await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
-  })
-
-  it('matches snapshot', async () => {
-    await expect(setup(text)).rejects.toMatchSnapshot()
-  })
-})
-
-describe('inconsistent right bound', () => {
-  const text = `
+  describe('inconsistent right bound', () => {
+    const text = `
     | id | name        | email                    |
     |----|-------------|--------------------------|
     |  1 | John Doe    | john-doe@gmail.com
@@ -36,17 +38,17 @@ describe('inconsistent right bound', () => {
     |  3 | Julia Jones | jjones778@gmail.com
   `
 
-  it('rejects with a SyntaxError', async () => {
-    await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    it('rejects with a SyntaxError', async () => {
+      await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', async () => {
+      await expect(setup(text)).rejects.toMatchSnapshot()
+    })
   })
 
-  it('matches snapshot', async () => {
-    await expect(setup(text)).rejects.toMatchSnapshot()
-  })
-})
-
-describe('inconsistent left bound', () => {
-  const text = `
+  describe('inconsistent left bound', () => {
+    const text = `
     | id | name        | email                    |
     |----|-------------|--------------------------|
        1 | John Doe    | john-doe@gmail.com       |
@@ -54,11 +56,76 @@ describe('inconsistent left bound', () => {
     |  3 | Julia Jones | jjones778@gmail.com      |
   `
 
-  it('rejects with a SyntaxError', async () => {
-    await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    it('rejects with a SyntaxError', async () => {
+      await expect(setup(text)).rejects.toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', async () => {
+      await expect(setup(text)).rejects.toMatchSnapshot()
+    })
+  })
+})
+
+describe('sync', () => {
+  const setup = (text: string) => tryExec(() => Array.from(createMarkdownObjectTableSync(text)))
+
+  describe('empty string', () => {
+    const text = ''
+
+    it('throws a SyntaxError', () => {
+      expect(setup(text).error).toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', () => {
+      expect(setup(text).error).toMatchSnapshot()
+    })
   })
 
-  it('matches snapshot', async () => {
-    await expect(setup(text)).rejects.toMatchSnapshot()
+  describe('string with naught but whitespaces', () => {
+    const text = '  \n  \t '
+
+    it('throws a SyntaxError', () => {
+      expect(setup(text).error).toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', () => {
+      expect(setup(text).error).toMatchSnapshot()
+    })
+  })
+
+  describe('inconsistent right bound', () => {
+    const text = `
+    | id | name        | email                    |
+    |----|-------------|--------------------------|
+    |  1 | John Doe    | john-doe@gmail.com
+    |  2 | Peter Smith | petersmith22@outlook.com
+    |  3 | Julia Jones | jjones778@gmail.com
+  `
+
+    it('throws a SyntaxError', async () => {
+      expect(setup(text).error).toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', () => {
+      expect(setup(text).error).toMatchSnapshot()
+    })
+  })
+
+  describe('inconsistent left bound', () => {
+    const text = `
+    | id | name        | email                    |
+    |----|-------------|--------------------------|
+       1 | John Doe    | john-doe@gmail.com       |
+       2 | Peter Smith | petersmith22@outlook.com |
+    |  3 | Julia Jones | jjones778@gmail.com      |
+  `
+
+    it('throws a SyntaxError', () => {
+      expect(setup(text).error).toBeInstanceOf(SyntaxError)
+    })
+
+    it('matches snapshot', () => {
+      expect(setup(text).error).toMatchSnapshot()
+    })
   })
 })
